3b0e26b6ab0314f25fef0d13573e2acf53d0c892
David An <dong82@gmail.com>
2021-01-28 16:40:22 -0500

    Added T9 for Angular 8 Test

---
+++ T9/ClientApp/src/app/Scroll/EmployeeDetail.ts
@@ -0,0 +1,15 @@
+export interface EmployeeDetail {
+  empNo: number;
+  firstName: string;
+  lastName: string;
+  // birthDate: string;
+  // hireDate: string;
+  // gender: string;
+  // salary: number;
+  title: string;
+  // deptNo: string;
+  // deptName: string;
+  // managerEmpNo: number;
+  // managerFirstName: string;
+  // managerLastName: string;
+}
---
+++ T9/ClientApp/src/app/Scroll/my.component.ts
@@ -0,0 +1,53 @@
+import { DataService } from './data.service';
+import { Component, OnInit } from '@angular/core';
+import { TableVirtualScrollDataSource } from 'ng-table-virtual-scroll';
+import { catchError, tap, map } from 'rxjs/operators';
+import { EmployeeDetail } from './EmployeeDetail';
+
+@Component({
+  selector: 'app-my',
+  templateUrl: './my.component.html',
+  styleUrls: ['./my.component.css']
+})
+export class MyComponent implements OnInit {
+  // dataSource: TableVirtualScrollDataSource<EmployeeDetail[]>;
+
+  // displayedColumns = ['empNo', 'firstName', 'lastName', 'title'];
+  // empNo: number;
+  // firstName: string;
+  // lastName: string;
+  // // birthDate: string;
+  // // hireDate: string;
+  // // gender: string;
+  // // salary: number;
+  // title: string;
+  // // deptNo: string;
+  // // deptName: string;
+  // // managerEmpNo: number;
+  // // managerFirstName: string;
+  // // managerLastName: string;
+
+  employeeDetails = [];
+
+  constructor(private dataService: DataService) { }
+
+  ngOnInit(): void {
+    this.dataService.getEmployeeDetails()
+      .pipe(
+        tap(data => console.log(JSON.stringify(data.length)))
+      )
+      .subscribe(
+        data => this.employeeDetails = data
+        // stream => {
+        //   this.dataSource.data = new TableVirtualScrollDataSource<EmployeeDetail>(stream);
+        // }
+      );
+
+    // this.dataService.getEmployeeDetails()
+    // .subscribe(
+    //   stream => {
+    //     this.dataSource = new TableVirtualScrollDataSource(stream);
+    //   }
+    // );
+  }
+}
