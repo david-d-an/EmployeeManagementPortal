258df1301d124a4757fb3fc2bc46e85b6dc56296
David An <dong82@gmail.com>
2021-02-11 16:44:13 -0500

    Not right way for security

--- EMP.Web/ClientApp/src/app/user/user.module.ts
+++ EMP.Web/ClientApp/src/app/user/user.module.ts
@@ -1,25 +1,25 @@
-import { EmpCommonModule } from '../shared/emp-common.module';
-// import { FormsModule } from '@angular/forms';
-// import { CommonModule } from '@angular/common';
-import { Routes, RouterModule } from '@angular/router';
-import { NgModule, Component } from '@angular/core';
+// import { EmpCommonModule } from '../shared/emp-common.module';
+// // import { FormsModule } from '@angular/forms';
+// // import { CommonModule } from '@angular/common';
+// import { Routes, RouterModule } from '@angular/router';
+// import { NgModule, Component } from '@angular/core';
 
-import { LoginComponent } from './login.component';
+// import { LoginComponent } from './login.component';
 
-// import { SharedModule } from '../shared/shared.module';
+// // import { SharedModule } from '../shared/shared.module';
 
-const routes: Routes = [
-  { path: 'login', component: LoginComponent  },
-];
+// const routes: Routes = [
+//   { path: 'login', component: LoginComponent  },
+// ];
 
-@NgModule({
-  imports: [
-    // SharedModule,
-    RouterModule.forChild(routes),
-    EmpCommonModule
-  ],
-  declarations: [
-    LoginComponent
-  ]
-})
-export class UserModule { }
+// @NgModule({
+//   imports: [
+//     // SharedModule,
+//     RouterModule.forChild(routes),
+//     EmpCommonModule
+//   ],
+//   declarations: [
+//     LoginComponent
+//   ]
+// })
+// export class UserModule { }
