e18123febde4b60d57e7c51ab41fd322d89a1113
David An <dong82@gmail.com>
2021-02-11 12:29:33 -0500

    Migration code for Employees DB

---
+++ EMP.DbScaffold/Migrations/Employees/20210211152430_UpdatedDb_2.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace EMP.DbScaffold.Migrations.Employees
+{
+    public partial class UpdatedDb_2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "City",
+                table: "departments",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "City",
+                table: "departments");
+        }
+    }
+}
---
+++ EMP.DbScaffold/Migrations/Employees/20210211155141_UpdatedDb_4.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace EMP.DbScaffold.Migrations.Employees
+{
+    public partial class UpdatedDb_4 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Zip",
+                table: "departments",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Zip",
+                table: "departments");
+        }
+    }
+}
