b0b51debc4a49557ab3afa9e0fdc8e819d9d9934
David An <dong82@gmail.com>
2021-04-30 21:24:01 -0400

    Debug cert gen script

--- Generate-Certificates.sh
+++ Generate-Certificates.sh
@@ -22,6 +22,7 @@
 
 # Generate private key
 openssl genrsa -des3 -out myCA.key 2048
+
 # Generate root certificate
 openssl req -x509 -new -nodes -key myCA.key -sha256 -days 825 -out myCA.pem
 
@@ -29,11 +30,15 @@ openssl req -x509 -new -nodes -key myCA.key -sha256 -days 825 -out myCA.pem
 # Create CA-signed certs
 ######################
 
-NAME=mydomain.com # Use your own domain name
+# Use your own domain name
+NAME=localhost
+
 # Generate a private key
 openssl genrsa -out $NAME.key 2048
+
 # Create a certificate-signing request
 openssl req -new -key $NAME.key -out $NAME.csr
+
 # Create a config file for the extensions
 >$NAME.ext cat <<-EOF
 authorityKeyIdentifier=keyid,issuer
@@ -45,6 +50,7 @@ DNS.1 = $NAME # Be sure to include the domain name here because Common Name is n
 DNS.2 = bar.$NAME # Optionally, add additional domains (I've added a subdomain here)
 IP.1 = 192.168.0.13 # Optionally, add an IP address (if the connection which you have planned requires it)
 EOF
-# Create the signed certificate
+
+# Create the signed certificate valid for 3650 days
 openssl x509 -req -in $NAME.csr -CA myCA.pem -CAkey myCA.key -CAcreateserial \
--out $NAME.crt -days 825 -sha256 -extfile $NAME.ext
+-out $NAME.crt -days 3650 -sha256 -extfile $NAME.ext
